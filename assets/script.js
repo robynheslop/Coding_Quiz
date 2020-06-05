e3c5a75afc802ce550a0b627695ac4c00a25c038
Robyn H <robyn.heslop@hotmail.com>
2020-06-05 19:31:43 +0800

    updates to questions

--- assets/script.js
+++ assets/script.js
@@ -26,34 +26,32 @@ var displayHighScores = document.querySelector("#highscores-displayed");
 // link to high score submission div
 var enterHS = document.querySelector("#enter-HS-displayed");
 
-// Changing elements 
-
-// create link to "answer options" - div encompassing buttons to click answer to q
-var answerButtonTargets = document.querySelector("#answer-buttons");
-
-// submit data button
-var submitDataButton = document.querySelector("#submitInitials");
-
-// create variable to access the display for "previous result" (correct or wrong)
-var previousResultText = document.querySelector("#lastResult");
-// access to value displayed for countdown
+// other links in HTML
+// access to time value displayed for countdown
 var currentTime = document.querySelector("#current-time");
-//create variable for submiting data button 
-var submittedInitials = document.querySelector("#initials");
-//link to submiting data button 
-var highScoreList = document.querySelector("#high-scores-list");
-// access text content of "questions" span
+// create link to "answer options" - div encompassing all buttons to click + answer q's
+var answerButtonTargets = document.querySelector("#answer-buttons");
+// link to access text content of "questions" heading2 and buttons
 var displayedQuestion = document.querySelector("#currentQuestion");
 var displayedOption1 = document.querySelector("#option1");
 var displayedOption2 = document.querySelector("#option2");
 var displayedOption3 = document.querySelector("#option3");
 var displayedOption4 = document.querySelector("#option4");
+// create variable to access the display for "previous result" (correct or wrong text)
+var previousResultText = document.querySelector("#lastResult");
+// link to submiting data button 
+var submitDataButton = document.querySelector("#submitInitials");
+// create variable linking to submitted initials
+var submittedInitials = document.querySelector("#initials");
+// link to ORDERED list high scores 
+var highScoreList = document.querySelector("#high-scores-list");
 
+// New variables
 // empty variable to loop through in question display
 var j = 0;
 // empty variable to hold the current correct answer
 var correctAnswer = "";
-// hgih scores start off empty
+// high scores var start off empty
 var highscores = [];
 // create var display time - initially empty
 var displayTime = "";
@@ -70,36 +68,36 @@ var questions = [
         correct_answer: "Alerts",
     },
     {
-        question: "Inside which HTML element do we put the JavaScript?",
-        answer1: "<script></script>",
-        answer2: "<javascript></javascript>",
-        answer3: "<scripting></scripting>",
-        answer4: "<js></js>",
-        correct_answer: "<script></script>",
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        answer1: "alertBox('Hello World')",
-        answer2: "alert('Hello World')",
-        answer3: "msgBox('Hello World')",
-        answer4: "msg('Hello World')",
-        correct_answer: "alert('Hello World')",
-    },
-    {
-        question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-        answer1: "if i <> 5",
-        answer2: "if (i !=5)",
-        answer3: "if (i <> 5)",
-        answer4: "if i =! 5 then",
-        correct_answer: "if (i !=5)",
-    },
-    {
-        question: "Arrays in Javascript can be used to store...",
+        question: "Arrays in JavaScript can be used to store ____.",
         answer1: "numbers and strings.",
-        answer2: "others arrays",
-        answer3: "booleans",
+        answer2: "other arrays.",
+        answer3: "booleans.",
         answer4: "all of the above",
         correct_answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answer1: "commas",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "parentheses",
+        correct_answer: "quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answer1: "JavaScript",
+        answer2: "terminal/bash",
+        answer3: "for loops",
+        answer4: "console.log",
+        correct_answer: "console.log",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed withing ____.",
+        answer1: "parentheses",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "square brackets",
+        correct_answer: "parentheses",
     }
 ];
 
@@ -255,8 +253,6 @@ function markQuestions() {
         borderAboveLastResult.style.visibility = "visible";
         previousResultText.textContent = "Correct!";
     }
-
-    // if correct answer is clicked, continue - no change to timer
 }
 
 // function if clicked answer is not correct answer 
